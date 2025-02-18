ac1e4d6b791a2f55cdb73355c465e05508650722
Zukhriddin Mamarasulov <Zukhriddin.Mamarasulov@kdb.uz>
2025-02-18 17:45:38 +0500

    Update repo

--- src/main/java/io/javalin/javalin_project/JavalinProject.java
+++ src/main/java/io/javalin/javalin_project/JavalinProject.java
@@ -1,11 +1,28 @@
 package io.javalin.javalin_project;
 
 import io.javalin.Javalin;
+import io.javalin.apibuilder.ApiBuilder;
+import io.javalin.javalin_project.config.Auth;
+import io.javalin.javalin_project.controller.UserController;
+import io.javalin.javalin_project.enums.Role;
 
 public class JavalinProject {
-    public static void main(String[] args) {
-		var app = Javalin.create(/*config*/)
-	            .get("/", ctx -> ctx.result("Javaline Test"))
-	            .start(8090);
-    }
+	public static void main(String[] args) {
+		Javalin app = Javalin.create(config -> {
+			config.router.mount(router -> {
+				router.beforeMatched(Auth::handleAccess);
+			}).apiBuilder(() -> {
+				ApiBuilder.get("/", ctx -> ctx.redirect("/users"), Role.ANYONE);
+				ApiBuilder.path("users", () -> {
+					ApiBuilder.get(UserController::getAllUserIds, Role.ANYONE);
+					ApiBuilder.post(UserController::createUser, Role.USER_WRITE);
+					ApiBuilder.path("{userId}", () -> {
+						ApiBuilder.get(UserController::getUser, Role.USER_READ);
+						ApiBuilder.patch(UserController::updateUser, Role.USER_WRITE);
+						ApiBuilder.delete(UserController::deleteUser, Role.USER_WRITE);
+					});
+				});
+			});
+		}).start(7070);
+	}
 }
---
+++ src/main/java/io/javalin/javalin_project/enums/Role.java
@@ -0,0 +1,5 @@
+package io.javalin.javalin_project.enums;
+
+import io.javalin.security.RouteRole;
+
+public enum Role implements RouteRole { ANYONE, USER_READ, USER_WRITE }
